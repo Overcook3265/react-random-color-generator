b21428c93cdc702fe765b884a0d9f8e0d351e9f2
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-01-15 23:51:11 +0100

    Add  alignment and styling of elements

--- src/App.js
+++ src/App.js
@@ -6,29 +6,48 @@ export default function App() {
 
   return (
     <>
-      <br />
-      <button
-        onClick={() => {
-          setRandCol(randomColor());
-        }}
-      >
-        Generate
-      </button>
-      <br />
-      <br />
       <div
         style={{
           backgroundColor: randCol,
+          borderRadius: 5,
           fontFamily: 'sans-serif',
           padding: 20,
-          maxWidth: 150,
-          minHeight: 50,
+          maxWidth: 200,
           textAlign: 'center',
         }}
       >
-        Generated Color: {randCol}
+        <br />
+        <button
+          onClick={() => {
+            setRandCol(randomColor());
+          }}
+          style={{
+            backgroundColor: '#424242',
+            borderColor: 'white',
+            borderRadius: 5,
+            color: 'white',
+            height: 50,
+            padding: 10,
+          }}
+        >
+          Generate
+        </button>
+        <br />
+        <br />
+        <div
+          style={{
+            backgroundColor: randCol,
+            fontFamily: 'sans-serif',
+            padding: 20,
+            maxWidth: 150,
+            minHeight: 50,
+            textAlign: 'center',
+          }}
+        >
+          Generated Color: {randCol}
+        </div>
+        <br />
       </div>
-      <br />
     </>
   );
 }
