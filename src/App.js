052106ac1db981e235a51cab5bc57a3fc972be8e
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-01-16 09:35:32 +0100

    Remove fragements

--- src/App.js
+++ src/App.js
@@ -5,49 +5,47 @@ export default function App() {
   const [randCol, setRandCol] = useState(randomColor());
 
   return (
-    <>
+    <div
+      style={{
+        backgroundColor: randCol,
+        borderRadius: 5,
+        fontFamily: 'sans-serif',
+        padding: 20,
+        maxWidth: 200,
+        textAlign: 'center',
+      }}
+    >
+      <br />
+      <button
+        onClick={() => {
+          setRandCol(randomColor());
+        }}
+        style={{
+          backgroundColor: '#424242',
+          borderColor: 'white',
+          borderRadius: 5,
+          color: 'white',
+          height: 50,
+          padding: 10,
+        }}
+      >
+        Generate
+      </button>
+      <br />
+      <br />
       <div
         style={{
           backgroundColor: randCol,
-          borderRadius: 5,
           fontFamily: 'sans-serif',
           padding: 20,
-          maxWidth: 200,
+          maxWidth: 150,
+          minHeight: 50,
           textAlign: 'center',
         }}
       >
-        <br />
-        <button
-          onClick={() => {
-            setRandCol(randomColor());
-          }}
-          style={{
-            backgroundColor: '#424242',
-            borderColor: 'white',
-            borderRadius: 5,
-            color: 'white',
-            height: 50,
-            padding: 10,
-          }}
-        >
-          Generate
-        </button>
-        <br />
-        <br />
-        <div
-          style={{
-            backgroundColor: randCol,
-            fontFamily: 'sans-serif',
-            padding: 20,
-            maxWidth: 150,
-            minHeight: 50,
-            textAlign: 'center',
-          }}
-        >
-          Generated Color: {randCol}
-        </div>
-        <br />
+        Generated Color: {randCol}
       </div>
-    </>
+      <br />
+    </div>
   );
 }
